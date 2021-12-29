ca550ff85a2fb5c56c43eb9007c402974cbabcb8
Jutta Koulen <jkoulen@gmail.com>
2021-12-29 11:54:28 +0100

    Updated Weather.js
    Removed CSS definitions (App.css and Weather.css to ensure intended formatting.

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 import "./App.css";
 
+
 export default function App() {
   return (
     <div className="App">
-      <div className="container"></div>
-      <h1 className="mb-5">Weather App</h1>
-      <Weather />
+      <div className="container">
+        <h1 className="mb-5">Weather App</h1>
+        <Weather />
 
-      <footer className="footer">
-        This project was coded by Reni Koulen, is
-        <a
-          href="https://github.com/renikoulen/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-        {" and is "}
-        <a
-          href="https://jolly-fermi-3c1a49.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+        <footer className="footer">
+          This project was coded by Reni Koulen, is
+          <a
+            href="https://github.com/renikoulen/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+          {" and is "}
+          <a
+            href="https://jolly-fermi-3c1a49.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -4,32 +4,47 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>				
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-control"
-        />
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          <li>6 °C | °F</li>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72 %</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary"
+              />
+            </div>
+          </div>
+        </form>
+        <h1> New York</h1>
+        <ul>
+          <li>Tuesday, 10:00</li>
+          <li>Partly cloudy</li>
+        </ul>
+
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly Cloudy"
+            />
+            6°C
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
